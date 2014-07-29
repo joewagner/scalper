50a01ddff142b35983a27d10c337a436cfcb63c0
Joe Wagner <njwjs722@gmail.com>
2014-07-29 15:24:11 -0600

    Add tests for init options

--- test/scalper.js
+++ test/scalper.js
@@ -1,13 +1,14 @@
 
 var Scalper = require('../index');
-var scalper = new Scalper();
+var MemoryStore = require('../memory-store');
 var should = require('should');
 
 describe('scalper', function () {
 
     describe('middleware', function () {
 
-        it('should exported', function (done) {
+        it('should be exported', function (done) {
+            var scalper = new Scalper();
             scalper.issueTickets.should.be.an.instanceOf(Function);
             done();
         });
@@ -26,16 +27,175 @@ describe('scalper', function () {
                 }
             };
 
+            var scalper = new Scalper();
             scalper.issueTickets()(req, res);
         });
 
     });
 
-
     it('should export a getter function', function (done) {
-        console.log(scalper.get);
+        var scalper = new Scalper();
         scalper.get.should.be.an.instanceOf(Function);
         done();
     });
 
-})
+    describe('authenticate option', function () {
+        it('should allow setting custom value', function (done) {
+            var count = 0;
+
+            var scalper = new Scalper({
+                authenticate: function (req) {
+                    count++;
+                    return true;
+                }
+            });
+
+            var req = {
+                method: 'GET',
+                url: '/socket-ticket'
+            };
+            var res = {
+                send: function (data) {
+                    should.exist(data.ticket);
+                    count.should.equal(1);
+                    done();
+                }
+            }
+            scalper.issueTickets()(req, res);
+        });
+
+        it('should default to checking req.user._id', function (done) {
+            var scalper = new Scalper();
+
+            var req = {
+                method: 'GET',
+                url: '/socket-ticket',
+                user: {
+                    _id: 'foo'
+                }
+            };
+            var res = {
+                send: function (data) {
+                    should.exist(data.ticket);
+                    done();
+                }
+            };
+            scalper.issueTickets()(req, res);
+        });
+
+        it('should default to checking req.user.id', function (done) {
+            var scalper = new Scalper();
+
+            var req = {
+                method: 'GET',
+                url: '/socket-ticket',
+                user: {
+                    id: 'foo'
+                }
+            };
+            var res = {
+                send: function (data) {
+                    should.exist(data.ticket);
+                    done();
+                }
+            };
+            scalper.issueTickets()(req, res);
+        });
+    });
+
+    describe('store option', function () {
+        it('should default to memory store', function (done) {
+            var scalper = new Scalper();
+
+            scalper.store.should.be.instanceOf(MemoryStore);
+            done();
+        });
+
+        it('should be configurable', function (done) {
+            var memStore = new MemoryStore();
+
+            var scalper = new Scalper({
+                store: memStore
+            });
+
+            var req = {
+                method: 'GET',
+                url: '/socket-ticket',
+                user: {
+                    id: 'foo'
+                }
+            };
+            var res = {
+                send: function (data) {
+                    should.exist(data.ticket);
+                    Object.keys(memStore.tickets).length.should.equal(1);
+                    done();
+                }
+            };
+            scalper.issueTickets()(req, res);
+        });
+    });
+
+    describe('route option', function () {
+        it('should default to `/socket-ticket`', function (done) {
+            var scalper = new Scalper();
+
+            scalper.route.should.equal('/socket-ticket');
+            done();
+        });
+
+        it('should be configurable', function (done) {
+            var scalper = new Scalper({
+                route: '/ticket-foo'
+            });
+
+            scalper.route.should.equal('/ticket-foo');
+
+            var req = {
+                method: 'GET',
+                url: '/socket-ticket',
+                user: {
+                    id: 'foo'
+                }
+            };
+            var res = {};
+
+            var middleware = scalper.issueTickets();
+            middleware(req, res, function () {
+                res.send = function (data) {
+                    should.exist(data.ticket);
+                    done();
+                };
+                req.url = '/ticket-foo';
+
+                middleware(req, res);
+            });
+        });
+    });
+
+    describe('genTicket option', function () {
+        it('should accept custom function', function (done) {
+            var scalper = new Scalper({
+                genTicket: function () {
+                    return 'custom-value';
+                }
+            });
+
+            var req = {
+                method: 'GET',
+                url: '/socket-ticket',
+                user: {
+                    id: 'foo'
+                }
+            };
+            var res = {
+                send: function (data) {
+                    should.exist(data.ticket);
+                    data.ticket.should.equal('custom-value');
+                    done();
+                }
+            }
+            scalper.issueTickets()(req, res);
+        });
+    });
+});
